6619a6a920ee418ede7b0c34508b2c45b13950c4
andrea-s21 <andreasisneros21@gmail.com>
2022-02-12 23:05:52 -0700

    merge to latest

--- main_app/views.py
+++ main_app/views.py
@@ -57,6 +57,7 @@ def generate_drink(request):
     for all_ids in all_drinks:
         ingredient_choice_ids = all_ids['idDrink']
         index_list.append(ingredient_choice_ids) 
+    # print(index_list)
 
     if request.POST['q1'] == ANSWERS1[0][0]:
         cocktails_by_category = requests.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=ordinary_drink').json()
@@ -69,17 +70,10 @@ def generate_drink(request):
         category_choice_ids = all_ids['idDrink']
         index_list2.append(category_choice_ids) 
     print(index_list2)
-
-    index_list3 = []
-    for id in index_list:
-      if id in index_list2:
-        index_list3.append(id)
-    print(index_list3)
-
-    drink_id = random.choice(index_list3)
-    print(drink_id)
-    final_drink_render = requests.get('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i={drink_id}').json()
-    print(final_drink_render)
+    # r = random.choice(cocktail['drinks'])
+    # drink_id = r['idDrink']
+    # print(all_drinks)
+    # print(r)
     return render(request, 'drinks/todays_cocktail.html', {
         'cocktails_by_ingredient': cocktails_by_ingredient,
         # 'drink_id': drink_id, 
